960f461df2fde370819a8847b42c2c84002c93b1
alex <aferreiraguido@gmail.com>
2022-06-05 12:08:22 +0200

    feat(app): agregando actuator y seguridad básica

--- src/main/java/com/example/testjava/infrastructure/webservice/PriceApi.java
+++ src/main/java/com/example/testjava/infrastructure/webservice/PriceApi.java
@@ -5,12 +5,12 @@ import com.example.testjava.application.service.PriceService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
-@Service
+@RestController
 @RequestMapping(value = "/v1/price")
 public class PriceApi {
     @Autowired
